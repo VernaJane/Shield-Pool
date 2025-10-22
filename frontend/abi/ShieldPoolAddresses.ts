9441b668c6f47109cb2b87758c65fb471b9a4985
AnselNeedham <buianhtuan48868@gmail.com>
2025-10-22 15:53:41 +0000

    Add ABI addresses

---
+++ frontend/abi/ShieldPoolAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const ShieldPoolAddresses = { 
+  "11155111": { address: "0x30249BE16CfF25247268BFD465A02751E3019b49", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x0000000000000000000000000000000000000000", chainId: 31337, chainName: "hardhat" },
+};
